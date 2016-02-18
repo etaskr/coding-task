ee708d4b0dde5a31ecd5a257172d2b6c43b1a8dc
sy8718 <shenyi_1987@hotmail.com>
2016-02-18 22:33:39 +1100

    init commit

---
+++ eTaskr/etaskr.Portal/Helper/Config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace etaskr.Portal.Helper
+{
+    public static class Config
+    {
+        private const string ForecastApiKeyKeyword = "ForecastApiKey";
+        private const string ReloadFrequencyKeyword = "ReloadFrequency";
+
+        public static string ForecastApiKey
+        {
+            get { return ConfigurationManager.AppSettings[ForecastApiKeyKeyword]; }
+        }
+
+        public static string ReloadFrequency
+        {
+            get { return ConfigurationManager.AppSettings[ReloadFrequencyKeyword]; }
+        }
+    }
+}
---
+++ eTaskr/etaskr.Portal/Scripts/Custom/Forecast.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    var lanF, longF;
+    var skycons = new Skycons();
+    function getForecast() {
+        $.ajax({
+            type: 'POST',
+            url: '/Home/GetForecast',
+            async: false,
+            data: {
+                lanF: lanF,
+                longF: longF
+            },
+            success: function (data) {
+                if (typeof data == "string") {
+                    alert(data);
+                    return;
+                }
+                initSkycons(data.currently.icon);
+                initTemperature(data.currently.temperature);
+                $('#loading').addClass("hide");
+                $('#content').removeClass("hide");
+            }
+        });
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success);
+    } else {
+        alert("Geo Location is not supported on your current browser!");
+    }
+
+    function success(position) {
+        lanF= position.coords.latitude;
+        longF = position.coords.longitude;
+        return getForecast();
+    }
+
+    function initSkycons(icon) {
+        $('#icon-title').text(icon);
+        skycons.set("icon", icon);
+        skycons.play();
+    }
+
+    function initTemperature(temperature) {
+        $('#temperature').text(Math.round(temperature) + "°C");
+    }
+
+    setInterval(getForecast, $('#reload-frequency').val());
+});
