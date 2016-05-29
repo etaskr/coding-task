0625721ba2ad0cb07b4f445b47779965f788e809
Hammy Goonan <hammy@spiresoftware.com.au>
2016-05-29 16:11:15 +1000

    basic API call working.

---
+++ api/index.js
@@ -0,0 +1,9 @@
+var api_req = require('./api_req')
+
+const api_key = '81ba32c3446a5a4012bd1bda9d074eae';
+const api_url = 'api.forecast.io';
+
+exports.getWeather = function(callback, lat, lng) {
+    var url = 'https://api.forecast.io/forecast/' + api_key + '/' + lat + ',' + lng;
+    return api_req.getRequest(url, callback);
+}
--- index.js
+++ index.js
@@ -1,11 +1,48 @@
 var express = require('express');
+var request = require('request');
+var async = require('async');
 var app = express();
+var api = require('./api');
+
+const api_key = '81ba32c3446a5a4012bd1bda9d074eae';
+const api_url = 'api.forecast.io';
 
 app.set('view engine', 'pug');
 app.use(express.static('static'));
 
 app.get('/', function (req, res) {
     res.render('index');
+});
+
+app.get('/api', function(req, res) {
+    var lat = req.query.lat
+    var lng = req.query.lng
+    if(!lat || !lng){
+        res.json(
+            {'error': {
+                'message': 'Please provide both a lat and lng value.'
+            }
+        });
+    }
+    async.waterfall(
+        [
+            function(callback){
+                api.getWeather(callback, lat, lng);
+            }
+        ],
+        function(error, response){
+            if (error) {
+                res.json(
+                    {'error':
+                        {'message': 'There was an error with your request'}
+                    }
+                );
+            }
+            else{
+                res.json(JSON.parse(response));
+            }
+        }
+    )
 });
 
 app.listen(3000, function () {
