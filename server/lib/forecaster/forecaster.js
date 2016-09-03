ed186a473c8ff18fb038977eba337d01d9488d32
Adam Kreitals <adam.kreitals@engagementinnovation.com>
2016-09-03 10:28:17 +1000

    add forecaster lib module to handle forecaster.io API calls

---
+++ server/lib/forecaster/forecaster.js
@@ -0,0 +1,65 @@
+// @flow
+
+'use strict';
+
+import axios from 'axios';
+import { BadRequest } from '../../helpers/errors';
+
+class Forecaster {
+    baseUrl: string;
+
+    /**
+     * Forecaster constructor
+     * @constructor
+     * @param {String} apiKey forecaster.io API key
+     */
+    constructor (apiKey: string) {
+        this.baseUrl = 'https://api.forecast.io/forecast/' + apiKey + '/';
+    }
+
+
+    /**
+     * get weather forcast for given lat long
+     * @param  {String} lat  latitude
+     * @param  {String} long longitude
+     * @param  {Object} options options to send to with forecast.io request
+     * @return {Promise}
+     */
+    getForecast (lat: string, long: string, options: Object = {}): Promise<Object> {
+        // check for valid lat long
+        if (!lat || !long) {
+            throw new BadRequest('Invalid location params');
+        }
+
+        const url = this.baseUrl + lat + ',' + long;
+
+        return this.makeRequest(url, options);
+    }
+
+
+    /**
+     * make forecast.io API request
+     * @param  {String} url request URL
+     * @param  {Object} params options to passed as quest string params
+     * @return {Promise}
+     */
+    makeRequest (url: string, params: Object): Promise<Object> {
+        const options = {
+            url: url,
+            params: params,        // forecase.io options
+            validateStatus: null        // don't automatically reject responses - allows for custom error
+        };
+
+        return axios.request(options)
+            .then((response) => {
+                if (response.status !== 200) {
+                    // NOTE: this error will be caught in the wrapping promises catch statement
+                    throw new BadRequest(response.data.error || 'request to forecast.io failed');
+                }
+
+                return response.data;
+            });
+    }
+}
+
+export default Forecaster;
---
+++ server/lib/forecaster/forecaster.spec.js
@@ -0,0 +1,56 @@
+/**
+ * Do not run flow on tests as we want to test error handling
+ */
+
+import { expect } from 'chai';
+import { BadRequest } from '../../helpers/errors';
+import Forecaster from './';
+
+// mock location (Melbourne)
+const mockLocation = {
+    lat: '-37.8053564',
+    long: '145.0658874'
+};
+
+// throw error if no API key is supplied
+if (!process.env.FORECASTIO_API_KEY) {
+    throw new Error('Usage: env FORECASTIO_API_KEY=<your api key> npm test');
+}
+
+// set up new forecaster
+const forecaster = new Forecaster(process.env.FORECASTIO_API_KEY);
+
+describe('Forecaster', function () {
+    it('should throw a BadRequest for an invalid lat and long', function () {
+        return expect(() => forecaster.getForecast('1', null)).to.throw(BadRequest);
+    });
+
+    it('should reject the promise with a BadRequest for a failed forecast.io response', function () {
+        return forecaster.getForecast(mockLocation.lat, '200')
+            .catch((err) => {
+                expect(err).to.be.instanceOf(BadRequest);
+            });
+    });
+
+    it('should return the forecast for a valid lat and long', function () {
+        return forecaster.getForecast(mockLocation.lat, mockLocation.long)
+            .then((data) => {
+                expect(data).to.exist;
+                expect(data).to.contain.all.keys('currently', 'hourly', 'daily', 'flags');
+            });
+    });
+
+    it('should pass options to forecast.io and return corresponding forecast', function () {
+        const options = {
+            units: 'si',
+            exclude: 'daily, hourly'
+        };
+
+        return forecaster.getForecast(mockLocation.lat, mockLocation.long, options)
+            .then((data) => {
+                expect(data).to.exist;
+                expect(data.flags.units).to.equal(options.units);
+                expect(data).not.to.contain.any.keys('hourly', 'daily');
+            });
+    });
+});
